768b0588593529639d3c7f554f691dc3b21b32d6
Cyril Rozario <cyrilrozario7@gmail.com>
2026-07-16 17:25:55 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/io/github/rozarioc33art/markdownnoteapp/MarkdownNoteAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.rozarioc33art.markdownnoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownNoteAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/rozarioc33art/markdownnoteapp/MarkdownNoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rozarioc33art.markdownnoteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNoteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
