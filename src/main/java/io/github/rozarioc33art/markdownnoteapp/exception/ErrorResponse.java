d97840ccb494baca23a21431bcac8ce612da1620
Cyril Rozario <cyrilrozario7@gmail.com>
2026-07-27 09:09:59 +0530

    Add global exception handling for note not found errors

---
+++ src/main/java/io/github/rozarioc33art/markdownnoteapp/exception/ErrorResponse.java
@@ -0,0 +1,12 @@
+package io.github.rozarioc33art.markdownnoteapp.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+
+}
--- src/main/java/io/github/rozarioc33art/markdownnoteapp/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/rozarioc33art/markdownnoteapp/exception/GlobalExceptionHandler.java
@@ -1,8 +1,20 @@
 package io.github.rozarioc33art.markdownnoteapp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoteNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoteNotFound(NoteNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
+    }
 
 }
